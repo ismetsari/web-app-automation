f2ad72ea72b6eb582a2a3d800bb7f5ae8b67e6a1
root <root@master.localdomain>
2024-05-18 20:54:00 +0300

    Test commit v2!

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=> {
-    res.send("Welcome to my nodejs app! v2")
+    res.send("Welcome to my nodejs app! v3")
 });
 
 app.listen(3000, function() {
