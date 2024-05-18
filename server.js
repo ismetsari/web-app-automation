eb556ddbc4bc8e73ac8b210c2d437ae8c66405f4
root <root@master.localdomain>
2024-05-18 21:09:23 +0300

    Last changes.

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=> {
-    res.send("Welcome to my nodejs app! v4")
+    res.send("Welcome to my nodejs app!")
 });
 
-app.listen(3000, function() {
-    console.log("New version of app listening on port 3000")
-});
