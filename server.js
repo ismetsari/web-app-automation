c9882da2f7f8fd3ef35203d7516b0daa559a39bc
root <root@master.localdomain>
2024-05-18 21:06:39 +0300

    Test v4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=> {
-    res.send("Welcome to my nodejs app! v3")
+    res.send("Welcome to my nodejs app! v4")
 });
 
 app.listen(3000, function() {
